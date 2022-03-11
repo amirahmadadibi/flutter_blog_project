9238ae7b1b56a129ad1684ca3bb75881e9da73a8
Amirahmad Adibi <amirahmadadibi@Amirahmads-MacBook-Pro.local>
2022-03-11 19:42:17 +0330

    week5 E02-basic of the project structure

---
+++ lib/main.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(HomeScreen());
+}
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: SafeArea(
+          child: Center(
+            child: TextButton(
+              onPressed: () {},
+              child: Text('Next'),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class SecondScreen extends StatelessWidget {
+  const SecondScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: SafeArea(
+          child: Center(
+            child: Text('AmriahmadAdibi'),
+          ),
+        ),
+      ),
+    );
+  }
+}
