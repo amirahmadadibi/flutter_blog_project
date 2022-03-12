677fe932717e7d3127e5241a381703342b6be38e
Amirahmad Adibi <amirahmadadibi@Amirahmads-MacBook-Pro.local>
2022-03-12 21:02:42 +0330

    week5 E13 solve changle scroolview

--- lib/main.dart
+++ lib/main.dart
@@ -25,40 +25,42 @@ class HomeScreen extends StatelessWidget {
     return Scaffold(
       backgroundColor: Colors.deepOrangeAccent,
       body: SafeArea(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text(
-              'خوش‌آمدید',
-              style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20.0),
-            ),
-            Image(
-              image: AssetImage('images/welcome.png'),
-            ),
-            OutlinedButton(
-              style: OutlinedButton.styleFrom(
+        child: SingleChildScrollView(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                'خوش‌آمدید',
+                style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20.0),
+              ),
+              Image(
+                image: AssetImage('images/welcome.png'),
+              ),
+              OutlinedButton(
+                style: OutlinedButton.styleFrom(
+                    minimumSize: Size(200.0, 40.0),
+                    side: BorderSide(color: Colors.white, width: 2.0),
+                    primary: Colors.white),
+                onPressed: () {},
+                child: Text(
+                  'ورود به حساب',
+                  style: TextStyle(fontSize: 16),
+                ),
+              ),
+              TextButton(
+                style: TextButton.styleFrom(
+                  primary: Colors.black,
+                  backgroundColor: Colors.white,
                   minimumSize: Size(200.0, 40.0),
-                  side: BorderSide(color: Colors.white, width: 2.0),
-                  primary: Colors.white),
-              onPressed: () {},
-              child: Text(
-                'ورود به حساب',
-                style: TextStyle(fontSize: 16),
-              ),
-            ),
-            TextButton(
-              style: TextButton.styleFrom(
-                primary: Colors.black,
-                backgroundColor: Colors.white,
-                minimumSize: Size(200.0, 40.0),
-              ),
-              onPressed: () {},
-              child: Text(
-                'ثبت نام',
-                style: TextStyle(fontSize: 16),
-              ),
-            )
-          ],
+                ),
+                onPressed: () {},
+                child: Text(
+                  'ثبت نام',
+                  style: TextStyle(fontSize: 16),
+                ),
+              )
+            ],
+          ),
         ),
       ),
     );
