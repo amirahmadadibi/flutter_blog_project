b2e2f99f08ab3b3d4dde707155b7e50850553055
Amirahmad Adibi <amirahmadadibi@Amirahmads-MacBook-Pro.local>
2022-03-19 00:54:48 +0330

    week5 E17-spliteCodeFileForTheFirstTIme

---
+++ lib/login_page.dart
@@ -0,0 +1,58 @@
+import 'package:flutter/material.dart';
+
+class LoginPage extends StatelessWidget {
+  const LoginPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.deepOrangeAccent,
+      body: SafeArea(
+        child: SingleChildScrollView(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              SizedBox(
+                height: 20,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    'خوش‌آمدید',
+                    style:
+                        TextStyle(fontWeight: FontWeight.bold, fontSize: 20.0),
+                  ),
+                  SizedBox(
+                    width: 14,
+                  ),
+                  Icon(Icons.login),
+                ],
+              ),
+              Image(
+                image: AssetImage('images/welcome.png'),
+              ),
+              OutlinedButton(
+                style: OutlinedButton.styleFrom(
+                    minimumSize: Size(200.0, 40.0),
+                    side: BorderSide(color: Colors.white, width: 2.0),
+                    primary: Colors.white),
+                onPressed: () {},
+                child: Text(
+                  'ورود به حساب',
+                  style: TextStyle(fontSize: 16),
+                ),
+              ),
+              ElevatedButton(
+                onPressed: () {},
+                child: Text('ثبت نام'),
+                style: ElevatedButton.styleFrom(
+                    primary: Colors.black, minimumSize: Size(200.0, 43.0)),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'login_page.dart';
 
 void main() {
   runApp(Application());
@@ -12,64 +13,7 @@ class Application extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       theme: ThemeData(fontFamily: 'vazir'),
-      home: HomeScreen(),
-    );
-  }
-}
-
-class HomeScreen extends StatelessWidget {
-  const HomeScreen({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.deepOrangeAccent,
-      body: SafeArea(
-        child: SingleChildScrollView(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              SizedBox(
-                height: 20,
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Text(
-                    'خوش‌آمدید',
-                    style:
-                        TextStyle(fontWeight: FontWeight.bold, fontSize: 20.0),
-                  ),
-                  SizedBox(
-                    width: 14,
-                  ),
-                  Icon(Icons.login),
-                ],
-              ),
-              Image(
-                image: AssetImage('images/welcome.png'),
-              ),
-              OutlinedButton(
-                style: OutlinedButton.styleFrom(
-                    minimumSize: Size(200.0, 40.0),
-                    side: BorderSide(color: Colors.white, width: 2.0),
-                    primary: Colors.white),
-                onPressed: () {},
-                child: Text(
-                  'ورود به حساب',
-                  style: TextStyle(fontSize: 16),
-                ),
-              ),
-              ElevatedButton(
-                onPressed: () {},
-                child: Text('ثبت نام'),
-                style: ElevatedButton.styleFrom(
-                    primary: Colors.black, minimumSize: Size(200.0, 43.0)),
-              )
-            ],
-          ),
-        ),
-      ),
+      home: LoginPage(),
     );
   }
 }
