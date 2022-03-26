653d06b7b0271387cd75e4fde3cfc88d49a033ab
Amirahmad Adibi <amirahmadadibi@Amirahmads-MacBook-Pro.local>
2022-03-26 19:45:13 +0430

    week5 E30-sovleChanlge

--- lib/login_page.dart
+++ lib/login_page.dart
@@ -18,59 +18,57 @@ class LoginPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.deepOrangeAccent,
+      backgroundColor: Colors.white,
       body: SafeArea(
-        child: SingleChildScrollView(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              SizedBox(
-                height: 20,
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Text(
-                    'خوش‌آمدید',
-                    style:
-                        TextStyle(fontWeight: FontWeight.bold, fontSize: 20.0),
-                  ),
-                  SizedBox(
-                    width: 14,
-                  ),
-                  Icon(Icons.login),
-                ],
-              ),
-              Image(
-                image: AssetImage('images/welcome.png'),
-              ),
-              OutlinedButton(
-                style: OutlinedButton.styleFrom(
-                    minimumSize: Size(200.0, 40.0),
-                    side: BorderSide(color: Colors.white, width: 2.0),
-                    primary: Colors.white),
-                onPressed: () {
-                  navigateToBlogPage(context, BlogScreen());
-                },
-                child: Text(
-                  'ورود به حساب',
-                  style: TextStyle(fontSize: 16),
+        child: Center(
+          child: SingleChildScrollView(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                SizedBox(
+                  height: 20,
                 ),
-              ),
-              ElevatedButton(
-                onPressed: () {},
-                child: Text('ثبت نام'),
-                style: ElevatedButton.styleFrom(
-                    primary: Colors.black, minimumSize: Size(200.0, 43.0)),
-              ),
-              TextButton(
-                onPressed: () {
-                  navigateToBlogPage(context, PasswordRecovery());
-                },
-                child: Text('فراموشی رمز عبور'),
-                style: TextButton.styleFrom(primary: Colors.white),
-              )
-            ],
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(
+                      'VIP ورود به حساب سینگنال ',
+                      style: TextStyle(
+                          fontWeight: FontWeight.bold, fontSize: 20.0),
+                    )
+                  ],
+                ),
+                Image(
+                  image: AssetImage('images/w.png'),
+                ),
+                OutlinedButton(
+                  style: OutlinedButton.styleFrom(
+                      minimumSize: Size(200.0, 40.0),
+                      side: BorderSide(color: Colors.black, width: 2.0),
+                      primary: Colors.black),
+                  onPressed: () {
+                    navigateToBlogPage(context, BlogScreen());
+                  },
+                  child: Text(
+                    'ورود به حساب',
+                    style: TextStyle(fontSize: 16),
+                  ),
+                ),
+                ElevatedButton(
+                  onPressed: () {},
+                  child: Text('ثبت نام'),
+                  style: ElevatedButton.styleFrom(
+                      primary: Colors.black, minimumSize: Size(200.0, 43.0)),
+                ),
+                TextButton(
+                  onPressed: () {
+                    navigateToBlogPage(context, PasswordRecovery());
+                  },
+                  child: Text('فراموشی رمز عبور'),
+                  style: TextButton.styleFrom(primary: Colors.grey),
+                )
+              ],
+            ),
           ),
         ),
       ),
